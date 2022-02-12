2704e0b61b7df33633bf57089b9b8b2d8e02df7b
mpaulosky <matthew.paulosky@outlook.com>
2022-02-11 19:03:15 -0800

    Add Data Models
    Added the data models for the project closes #2

---
+++ src/IssueTrackerLibrary/Models/IssueModel.cs
@@ -0,0 +1,15 @@
+﻿namespace IssueTrackerLibrary.Models;
+
+public class IssueModel
+{
+	[BsonId]
+	[BsonRepresentation(BsonType.ObjectId)]
+	public string Id { get; set; }
+	public string Issue { get; set; }
+	public string Description { get; set; }
+	public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+	public BasicUserModel Author { get; set; }
+	public StatusModel IssueStatus { get; set; }
+	public string OwnerNotes { get; set; }
+	public bool Archived { get; set; } = false;
+}
