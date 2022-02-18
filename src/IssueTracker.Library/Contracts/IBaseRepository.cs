1e8333684d6d835f6d42c66a8e76cd484bba72cc
mpaulosky <matthew.paulosky@outlook.com>
2022-02-18 00:26:09 -0800

    Create the repository structure

---
+++ src/IssueTracker.Library/Contracts/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace IssueTrackerLibrary.Contracts;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+	Task<TEntity> Get(string id);
+	Task<IEnumerable<TEntity>> Get();
+	Task Create(TEntity obj);
+	Task Update(string id, TEntity obj);
+	// void Delete(string id);
+}
---
+++ src/IssueTracker.Library/Contracts/IUserRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace IssueTrackerLibrary.Contracts;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+	Task<User> GetUserFromAuthentication(string id);
+}
