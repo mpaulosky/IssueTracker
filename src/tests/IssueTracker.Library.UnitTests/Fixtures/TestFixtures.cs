22429934abe810f326432321328a3e128e2a39f5
mpaulosky <matthew.paulosky@outlook.com>
2022-02-18 00:22:14 -0800

    Add Unit Tests
    Add IssueTracker.Library.UnitTests.csproj and tests for the MongoDbContextTests.cs the UserRepositoryTests.cs and the MongoUsersServiceTests.cs

---
+++ src/tests/IssueTracker.Library.UnitTests/Fixtures/TestFixtures.cs
@@ -0,0 +1,76 @@
+﻿using IssueTrackerLibrary.Models;
+
+using MongoDB.Bson;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IssueTrackerLibraryUnitTests.Fixtures;
+
+[ExcludeFromCodeCoverage]
+public static class TestFixtures
+{
+	public static IEnumerable<User> GetUsers()
+	{
+		var expected = new List<User>
+		{
+			new()
+			{
+				Id = Guid.NewGuid().ToString(),
+				ObjectIdentifier = Guid.NewGuid().ToString(),
+				FirstName = "Jim",
+				LastName = "Test",
+				DisplayName = "jimtest",
+				EmailAddress = "jim.test@test.com",
+				AuthoredIssues = new List<BasicIssueModel>(),
+				VotedOnComments = new List<BasicCommentModel>(),
+				AuthoredComments = new List<BasicCommentModel>()
+			},
+			new()
+			{
+				Id = Guid.NewGuid().ToString(),
+				ObjectIdentifier = Guid.NewGuid().ToString(),
+				FirstName = "Sam",
+				LastName = "Test",
+				DisplayName = "samtest",
+				EmailAddress = "sam.test@test.com",
+				AuthoredIssues = new List<BasicIssueModel>(),
+				VotedOnComments = new List<BasicCommentModel>(),
+				AuthoredComments = new List<BasicCommentModel>()
+			},
+			new ()
+			{
+				Id = Guid.NewGuid().ToString(),
+				ObjectIdentifier = Guid.NewGuid().ToString(),
+				FirstName = "Tim",
+				LastName = "Test",
+				DisplayName = "timtest",
+				EmailAddress = "tim.test@test.com",
+				AuthoredIssues = new List<BasicIssueModel>(),
+				VotedOnComments = new List<BasicCommentModel>(),
+				AuthoredComments = new List<BasicCommentModel>()
+			},
+		};
+		
+		return expected;
+	}
+
+	public static User GetUser(string userId, string objectIdentifier, string firstName, string lastName, string displayName, string email)
+	{
+		var expected = new User()
+		{
+			Id = userId,
+			ObjectIdentifier = objectIdentifier,
+			FirstName = firstName,
+			LastName = lastName,
+			DisplayName = "jimtest",
+			EmailAddress = "jim.test@test.com",
+			AuthoredIssues = new List<BasicIssueModel>(),
+			VotedOnComments = new List<BasicCommentModel>(),
+			AuthoredComments = new List<BasicCommentModel>()
+		};
+
+		return expected;
+	}
+}
---
+++ src/tests/IssueTracker.Library.UnitTests/MongoUsersServiceTests/MongoUsersServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using FluentAssertions;
+
+using IssueTrackerLibrary.Contracts;
+using IssueTrackerLibrary.DataAccess;
+using IssueTrackerLibrary.Models;
+
+using Microsoft.Extensions.Options;
+
+using NSubstitute;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace IssueTrackerLibraryUnitTests.MongoUsersDataTests;
+
+[ExcludeFromCodeCoverage]
+public class MongoUsersServiceTests
+{
+	private IOptions<IssueTrackerDatabaseSettings> _options;
+
+	public MongoUsersServiceTests()
+	{
+		var settings = new IssueTrackerDatabaseSettings()
+		{
+			DatabaseName = "TestDb", ConnectionString = "mongodb://tes123"
+		};
+
+		_options = Options.Create(settings);
+
+	}
+	
+	/*
+	[Fact()]
+	public async Task GetUsers_With_StateUnderTest_Should_Return_A_List_of_Users_Test()
+	{
+		// Arrange
+		var _context = Substitute.For<MongoDbContext>(_options);
+		
+		var expected = new List<UserModel>
+		{
+			new()
+			{
+				Id = Guid.NewGuid().ToString(),
+				ObjectIdentifier = Guid.NewGuid().ToString(),
+				FirstName = "Jim",
+				LastName = "Test",
+				DisplayName = "jimtest",
+				EmailAddress = "jim.test@test.com",
+				AuthoredIssues = new List<BasicIssueModel>(),
+				VotedOnComments = new List<BasicCommentModel>(),
+				AuthoredComments = new List<BasicCommentModel>()
+			},
+			new()
+			{
+				Id = Guid.NewGuid().ToString(),
+				ObjectIdentifier = Guid.NewGuid().ToString(),
+				FirstName = "Sam",
+				LastName = "Test",
+				DisplayName = "samtest",
+				EmailAddress = "sam.test@test.com",
+				AuthoredIssues = new List<BasicIssueModel>(),
+				VotedOnComments = new List<BasicCommentModel>(),
+				AuthoredComments = new List<BasicCommentModel>()
+			},
+			new ()
+			{
+				Id = Guid.NewGuid().ToString(),
+				ObjectIdentifier = Guid.NewGuid().ToString(),
+				FirstName = "Tim",
+				LastName = "Test",
+				DisplayName = "timtest",
+				EmailAddress = "tim.test@test.com",
+				AuthoredIssues = new List<BasicIssueModel>(),
+				VotedOnComments = new List<BasicCommentModel>(),
+				AuthoredComments = new List<BasicCommentModel>()
+			},
+		};
+		await _context.UserCollection.InsertManyAsync(expected).ConfigureAwait(false);
+		
+		var _sut = new MongoUserService(_context);
+
+		// Act
+		var users = await _sut.GetUsers().ConfigureAwait(false);
+		
+		// Assert
+		users.Should().NotBeNull();
+	}
+*/
+}
