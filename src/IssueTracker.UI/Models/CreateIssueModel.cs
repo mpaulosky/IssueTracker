ce44e3aca0a4cf800cbc312f4ad5ff0871b0b070
mpaulosky <matthew.paulosky@outlook.com>
2022-03-23 11:34:34 -0700

    Create and Update
    Create and Update the CreateIssueModel.cs Closes #20

--- src/IssueTracker.UI/Models/CreateIssueModel.cs
+++ src/IssueTracker.UI/Models/CreateIssueModel.cs
@@ -9,10 +9,6 @@ public class CreateIssueModel
 	public string Issue { get; set; }
 
 	[Required]
-	[MinLength(1)]
-	[Display(Name = "Category")]
-	public string CategoryId { get; set; }
-
 	[MaxLength(500)]
 	public string Description { get; set; }
 }
