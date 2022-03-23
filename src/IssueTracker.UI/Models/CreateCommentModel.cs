7b7a1d973edb6fbd94835a8e1c6161cc3934e277
mpaulosky <matthew.paulosky@outlook.com>
2022-03-23 11:38:32 -0700

    Create Comment 20-feature-add-create-issue-model Cretate the CreateCommentModel.cs which closes #21

---
+++ src/IssueTracker.UI/Models/CreateCommentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTrackerUI.Models;
+
+public class CreateCommentModel
+{
+	[Required]
+	[MaxLength(100)]
+	public string Comment { get; set; }
+	
+	[Required]
+	[MaxLength(500)]
+	public string Description { get; set; }
+}
