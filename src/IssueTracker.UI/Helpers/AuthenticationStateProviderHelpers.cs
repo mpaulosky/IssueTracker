43f993c8b56542c251065f2fc755984e468e23e8
mpaulosky <matthew.paulosky@outlook.com>
2022-03-23 10:07:14 -0700

    Validate and format

--- src/IssueTracker.UI/Helpers/AuthenticationStateProviderHelpers.cs
+++ src/IssueTracker.UI/Helpers/AuthenticationStateProviderHelpers.cs
@@ -6,16 +6,13 @@ namespace IssueTrackerUI.Helpers;
 
 public static class AuthenticationStateProviderHelpers
 {
-
 	public static async Task<User> GetUserFromAuth(
 		this AuthenticationStateProvider provider,
 		IUserService userData)
 	{
-
 		var authState = await provider.GetAuthenticationStateAsync();
 		string objectId = authState.User.Claims
 			.FirstOrDefault(c => c.Type.Contains("objectidentifier"))?.Value;
 		return await userData.GetUserFromAuthentication(objectId);
-
 	}
 }
