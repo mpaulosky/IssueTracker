68f9dd9f02d18ffd675b7edd0eb7449a5d0a4d38
mpaulosky <matthew.paulosky@outlook.com>
2022-03-30 13:49:09 -0700

    Add CreateCommentModel.cs, and CreateIssueModel.cs to UI project

--- src/IssueTracker.UI/Models/CreateCommentModel.cs
+++ src/IssueTracker.UI/Models/CreateCommentModel.cs
@@ -1,14 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace IssueTrackerUI.Models;
+namespace IssueTracker.UI.Models;
 
 public class CreateCommentModel
 {
+	public CreateCommentModel(string comment, string description)
+	{
+		Comment = comment ?? throw new ArgumentNullException(nameof(comment));
+		Description = description ?? throw new ArgumentNullException(nameof(description));
+	}
+
 	[Required]
 	[MaxLength(100)]
-	public string Comment { get; set; }
+	public string Comment { get; }
 	
 	[Required]
 	[MaxLength(500)]
-	public string Description { get; set; }
+	public string Description { get; init; }
 }
--- src/IssueTracker.UI/Models/CreateIssueModel.cs
+++ src/IssueTracker.UI/Models/CreateIssueModel.cs
@@ -1,14 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace IssueTrackerUI.Models;
+namespace IssueTracker.UI.Models;
 
 public class CreateIssueModel
 {
+	public CreateIssueModel(string issue, string description)
+	{
+		Issue = issue ?? throw new ArgumentNullException(nameof(issue));
+		Description = description ?? throw new ArgumentNullException(nameof(description));
+	}
+
 	[Required]
 	[MaxLength(75)]
-	public string Issue { get; set; }
+	public string Issue { get; }
 
 	[Required]
 	[MaxLength(500)]
-	public string Description { get; set; }
+	public string Description { get; }
 }
