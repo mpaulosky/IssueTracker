5d5e0bb3a299674e3762d9b4530639d6c2dc1374
mpaulosky <matthew.paulosky@outlook.com>
2022-03-30 14:10:12 -0700

    Add Missing Usings

--- src/IssueTracker.UI/GlobalUsings.cs
+++ src/IssueTracker.UI/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using IssueTracker.Library.Contracts;
 global using IssueTracker.Library.Models;
 global using IssueTracker.Library.DataAccess;
+global using IssueTracker.Library.Helpers;
+global using IssueTracker.Library.Services;
 global using IssueTracker.UI.Helpers;
