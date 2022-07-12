5f787494aae7d5780aab988124eb04a150523896
mpaulosky <matthew.paulosky@outlook.com>
2022-07-12 01:02:01 -0700

    Start Adding Create Page

--- src/IssueTracker.UI/Models/CreateIssueModel.cs
+++ src/IssueTracker.UI/Models/CreateIssueModel.cs
@@ -4,17 +4,11 @@ namespace IssueTracker.UI.Models;
 
 public class CreateIssueModel
 {
-	public CreateIssueModel(string issue, string description)
-	{
-		Issue = issue ?? throw new ArgumentNullException(nameof(issue));
-		Description = description ?? throw new ArgumentNullException(nameof(description));
-	}
-
 	[Required]
 	[MaxLength(75)]
-	public string Issue { get; }
+	public string Issue { get; set; }
 
 	[Required]
 	[MaxLength(500)]
-	public string Description { get; }
+	public string Description { get; set; }
 }
