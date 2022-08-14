ee4379673085536ff5fe51b4a54945cb6afe1b28
mpaulosky <matthew.paulosky@outlook.com>
2022-08-14 13:33:46 -0700

    Document MongoDbContext.cs

--- src/IssueTracker.Library/DataAccess/MongoDbContext.cs
+++ src/IssueTracker.Library/DataAccess/MongoDbContext.cs
@@ -2,8 +2,15 @@
 
 namespace IssueTracker.Library.DataAccess;
 
+/// <summary>
+/// MongoDbContext class
+/// </summary>
 public class MongoDbContext : IMongoDbContext
 {
+	/// <summary>
+	/// MongoDbContext constructor
+	/// </summary>
+	/// <param name="configuration">IOptions of DatabaseSettings</param>
 	public MongoDbContext(IOptions<DatabaseSettings> configuration)
 	{
 		DbName = configuration.Value.DatabaseName;
@@ -15,6 +22,12 @@ public class MongoDbContext : IMongoDbContext
 	public IMongoClient Client { get; }
 	public string DbName { get; }
 
+	/// <summary>
+	/// GetCollection method
+	/// </summary>
+	/// <param name="name">string</param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns>IMongoCollection</returns>
 	public IMongoCollection<T> GetCollection<T>(string name)
 	{
 		return Database.GetCollection<T>(name);
