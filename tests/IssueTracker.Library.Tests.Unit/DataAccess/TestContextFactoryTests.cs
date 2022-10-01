c6b5fb67d3891e4a10d6b50a5155f9a5838dfc14
mpaulosky <matthew.paulosky@outlook.com>
2022-10-01 10:17:14 -0700

    Add Unit Tests for TestContextFactory.cs
    Close #86

---
+++ tests/IssueTracker.Library.Tests.Unit/DataAccess/TestContextFactoryTests.cs
@@ -0,0 +1,61 @@
+﻿namespace IssueTracker.Library.DataAccess;
+
+public class TestContextFactoryTests
+{
+
+	private readonly IOptions<DatabaseSettings> _options;
+
+	public TestContextFactoryTests()
+	{
+
+		_options = TestFixtures.Settings();
+
+	}
+
+	[Fact]
+	public void TestContextFactory_With_Valid_Data_Should_Return_A_Context_Test()
+	{
+		// Arrange
+
+		// Act
+
+		var context = Substitute.For<TestContextFactory>(_options);
+
+		// Assert
+
+		context.Should().NotBeNull();
+		context.Client.Should().NotBeNull();
+		context.DbName.Should().Be("TestDb");
+	}
+
+	[Fact]
+	public void GetCollection_With_EmptyName_Should_Fail_Test()
+	{
+		// Arrange
+
+		// Act
+
+		var context = Substitute.For<TestContextFactory>(_options);
+
+		// Assert
+
+		Assert.Throws<ArgumentException>(() => context.GetCollection<UserModel>(""));
+	}
+
+	[Fact]
+	public void GetCollection_With_ValidName_Should_ReturnACollection_Test()
+	{
+		// Arrange
+
+		// Act
+
+		var context = Substitute.For<TestContextFactory>(_options);
+		var myCollection =
+			context.GetCollection<UserModel>(GetCollectionName(nameof(UserModel)));
+
+		// Assert
+
+		myCollection.Should().NotBeNull();
+		myCollection.CollectionNamespace.CollectionName.Should().BeSameAs("users");
+	}
+}
