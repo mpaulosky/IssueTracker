29181a409b241ce1294fb98b4a216caa737681da
mpaulosky <matthew.paulosky@gmail.com>
2023-03-14 14:07:43 -0800

    Add Partial Classes for AddAuthorizationAuthenticationService.cs, RegisterDatabaseContext.cs and RegisterDICollections.cs and Modify RegisterServices to use them

---
+++ src/IssueTracker.UI/Extensions/RegisterDICollections.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RegisterDICollections.cs" company="mpaulosky">
+//		Author:  Matthew Paulosky
+//		Copyright (c) 2022. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace IssueTracker.UI.Extensions;
+
+/// <summary>
+/// IServiceCollectionExtensions
+/// </summary>
+public static partial class IServiceCollectionExtensions
+{
+
+	/// <summary>
+	/// Register DI Collections
+	/// </summary>
+	/// <param name="services">IServiceCollection</param>
+	/// <returns>IServiceCollection</returns>
+	public static IServiceCollection RegisterDICollections(this IServiceCollection services)
+	{
+
+		services.AddSingleton<ICategoryService, CategoryService>();
+		services.AddSingleton<ICommentService, CommentService>();
+		services.AddSingleton<IStatusService, StatusService>();
+		services.AddSingleton<IIssueService, IssueService>();
+		services.AddSingleton<IUserService, UserService>();
+
+		services.AddSingleton<ICategoryRepository, CategoryRepository>();
+		services.AddSingleton<ICommentRepository, CommentRepository>();
+		services.AddSingleton<IStatusRepository, StatusRepository>();
+		services.AddSingleton<IIssueRepository, IssueRepository>();
+		services.AddSingleton<IUserRepository, UserRepository>();
+
+		return services;
+
+	}
+
+}
--- src/IssueTracker.UI/RegisterServices.cs
+++ src/IssueTracker.UI/RegisterServices.cs
@@ -5,6 +5,8 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+using IssueTracker.UI.Extensions;
+
 namespace IssueTracker.UI;
 
 /// <summary>
@@ -22,46 +24,20 @@ public static class RegisterServices
 	{
 		// Add services to the container.
 
-		builder.Services.AddRazorPages();
-
 		builder.Services.AddServerSideBlazor().AddMicrosoftIdentityConsentHandler();
+
+		builder.Services.AddAuthorizationAuthenticationServices(config);
+
+		builder.Services.AddRazorPages();
 
 		builder.Services.AddMemoryCache();
 
 		builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
 
-		builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-			.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
-
-		builder.Services.AddAuthorization(options =>
-		{
-			options.AddPolicy("Admin", policy =>
-			{
-				policy.RequireClaim("jobTitle", "Admin");
-			});
-		});
-
 		builder.Services.AddBlazoredSessionStorage();
 
-		// Setup DI
+		builder.Services.RegisterDatabaseContext(config);
 
-		builder.Services.AddSingleton<IMongoDbContextFactory>(_ =>
-		new MongoDbContextFactory
-		(
-			config.GetValue<string>("MongoDbSettings:ConnectionString"),
-			config.GetValue<string>("MongoDbSettings:DatabaseName"))
-		);
-
-		builder.Services.AddSingleton<ICategoryService, CategoryService>();
-		builder.Services.AddSingleton<ICommentService, CommentService>();
-		builder.Services.AddSingleton<IStatusService, StatusService>();
-		builder.Services.AddSingleton<IIssueService, IssueService>();
-		builder.Services.AddSingleton<IUserService, UserService>();
-
-		builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
-		builder.Services.AddSingleton<ICommentRepository, CommentRepository>();
-		builder.Services.AddSingleton<IStatusRepository, StatusRepository>();
-		builder.Services.AddSingleton<IIssueRepository, IssueRepository>();
-		builder.Services.AddSingleton<IUserRepository, UserRepository>();
+		builder.Services.RegisterDICollections();
 	}
 }
