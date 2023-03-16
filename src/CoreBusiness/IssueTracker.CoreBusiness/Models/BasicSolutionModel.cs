e0e31897670dac625ec9b256a08359dc074f6f95
mpaulosky <matthew.paulosky@gmail.com>
2023-03-16 11:45:46 -0700

    Add SolutionModel.cs and BasicSolutionModel.cs

---
+++ src/CoreBusiness/IssueTracker.CoreBusiness/Models/BasicSolutionModel.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------
+// <copyright File="BasicSolutionModel.cs"
+//	Company="mpaulosky">
+//	Author: Matthew Paulosky
+//	Copyright (c) 2022. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace IssueTracker.CoreBusiness.Models;
+
+public class BasicSolutionModel
+{
+
+	public BasicSolutionModel()
+	{
+	}
+
+	public BasicSolutionModel(SolutionModel solution)
+	{
+
+		Id = solution.Id;
+		SolutionTitle = solution.SolutionTitle;
+		Solution = solution.Solution;
+		Issue = solution.Issue;
+		Author = solution.Author;
+
+	}
+
+	public string Id { get; set; } = string.Empty;
+
+	public string SolutionTitle { get; set; } = string.Empty;
+
+	public string Solution { get; set; } = string.Empty;
+
+	public BasicIssueModel Issue { get; set; } = new BasicIssueModel();
+
+	public BasicUserModel Author { get; set; } = new();
+
+}
---
+++ src/CoreBusiness/IssueTracker.CoreBusiness/Models/SolutionModel.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright File="SolutionModel.cs"
+//	Company="mpaulosky">
+//	Author: Matthew Paulosky
+//	Copyright (c) 2022. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace IssueTracker.CoreBusiness.Models;
+
+public class SolutionModel
+{
+
+	[BsonId]
+	[BsonRepresentation(BsonType.ObjectId)]
+	public string Id { get; set; } = string.Empty;
+
+	[BsonElement("solution_title")]
+	[BsonRepresentation(BsonType.String)]
+	public string SolutionTitle { get; set; } = string.Empty;
+
+	[BsonElement("solution")]
+	[BsonRepresentation(BsonType.String)]
+	public string Solution { get; set; } = string.Empty;
+
+	[BsonElement("date_created")]
+	[BsonRepresentation(BsonType.DateTime)]
+	public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+	public BasicIssueModel Issue { get; set; } = new();
+
+	public BasicUserModel Author { get; set; } = new();
+
+	public HashSet<string> UserVotes { get; set; } = new();
+
+	[BsonElement("archived")]
+	[BsonRepresentation(BsonType.Boolean)]
+	public bool Archived { get; set; } = false;
+
+}
