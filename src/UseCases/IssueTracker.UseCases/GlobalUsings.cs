eca90d5fe9ad860c0f5de8267392a226ce0575c3
mpaulosky <matthew.paulosky@gmail.com>
2023-03-16 12:23:56 -0700

    Add Solution Level .editconfig

--- src/UseCases/IssueTracker.UseCases/GlobalUsings.cs
+++ src/UseCases/IssueTracker.UseCases/GlobalUsings.cs
@@ -1,3 +1,8 @@
 ﻿global using IssueTracker.CoreBusiness.Models;
-global using IssueTracker.UseCases.PlugInRepositories;
+global using IssueTracker.UseCases.Category.Interfaces;
+global using IssueTracker.UseCases.Comment.Interfaces;
+global using IssueTracker.UseCases.PlugInRepositoryInterfaces;
+global using IssueTracker.UseCases.Status.Interfaces;
 global using IssueTracker.UseCases.Users.Interfaces;
+
+
---
+++ src/UseCases/IssueTracker.UseCases/Solution/CreateNewSolutionUseCase.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//	File:		CreateNewSolutionUseCase.cs
+//	Company:mpaulosky
+//	Author:	Matthew Paulosky
+//	Copyright (c) 2022. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using IssueTracker.UseCases.Solution.Interfaces;
+
+namespace IssueTracker.UseCases.Solution;
+
+public class CreateNewSolutionUseCase : ICreateNewSolutionUseCase
+{
+
+	private readonly ISolutionRepository _solutionRepository;
+
+	public CreateNewSolutionUseCase(ISolutionRepository solutionRepository)
+	{
+
+		_solutionRepository = solutionRepository;
+
+	}
+
+	public async Task ExecuteAsync(SolutionModel solution)
+	{
+
+		if (solution == null) return;
+
+		await _solutionRepository.CreateNewSolutionAsync(solution);
+
+	}
+
+}
