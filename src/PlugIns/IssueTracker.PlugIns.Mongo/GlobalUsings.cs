01f69fec86016f68b231e13afd6e3fc7c9418d4b
mpaulosky <matthew.paulosky@gmail.com>
2023-03-17 12:07:16 -0700

    Add and Update Global Usings

---
+++ src/PlugIns/IssueTracker.PlugIns.Mongo/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using Ardalis.GuardClauses;
+
+global using IssueTracker.CoreBusiness.Models;
+global using IssueTracker.PlugIns.Mongo.Contracts;
+global using IssueTracker.UseCases.PlugInRepositoryInterfaces;
+
+global using MongoDB.Bson;
+global using MongoDB.Driver;
+
+global using static IssueTracker.PlugIns.Mongo.CollectionNames;
