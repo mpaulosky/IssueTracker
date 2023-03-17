ccb716baa1888bb94b8409bf7afa4798fdde0f6f
mpaulosky <matthew.paulosky@gmail.com>
2023-03-17 12:54:10 -0700

    Move Bogus Fakes to CoreBusiness

---
+++ src/CoreBusiness/IssueTracker.CoreBusiness/BogusFakes/FakeComment.cs
@@ -0,0 +1,72 @@
+//-----------------------------------------------------------------------
+// <copyright file="FakeComment.cs" company="mpaulosky">
+//		Author:  Matthew Paulosky
+//		Copyright (c) 2022. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace IssueTracker.CoreBusiness.BogusFakes;
+
+public static class FakeComment
+{
+
+	public static CommentModel GetNewComment()
+	{
+
+		Faker<CommentModel> commentsGenerator = new Faker<CommentModel>()
+		.RuleFor(c => c.Comment, f => f.Lorem.Sentence())
+		.RuleFor(x => x.Issue, FakeIssue.GetBasicIssues(1).First())
+		.RuleFor(c => c.Author, FakeUser.GetBasicUser(1).First())
+		.RuleFor(c => c.DateCreated, f => f.Date.Past());
+
+		CommentModel comment = commentsGenerator.Generate();
+
+		return comment;
+
+	}
+
+	public static IEnumerable<CommentModel> GetComments(int numberOfComments)
+	{
+
+		Faker<CommentModel> commentsGenerator = new Faker<CommentModel>()
+		.RuleFor(x => x.Id, Guid.NewGuid().ToString)
+		.RuleFor(c => c.Comment, f => f.Lorem.Sentence())
+		.RuleFor(x => x.Issue, FakeIssue.GetBasicIssues(1).First())
+		.RuleFor(c => c.Author, FakeUser.GetBasicUser(1).First())
+		.RuleFor(c => c.DateCreated, f => f.Date.Past());
+
+		List<CommentModel> comments = commentsGenerator.Generate(numberOfComments);
+
+		return comments;
+
+	}
+
+	public static IEnumerable<BasicCommentModel> GetBasicComments(int numberOfComments)
+	{
+
+		Faker<BasicCommentModel> commentsGenerator = new Faker<BasicCommentModel>()
+			.RuleFor(x => x.Id, Guid.NewGuid().ToString)
+			.RuleFor(c => c.Comment, f => f.Lorem.Sentence());
+
+		List<BasicCommentModel> basicComments = commentsGenerator.Generate(numberOfComments);
+
+		return basicComments;
+
+	}
+
+	public static BasicCommentModel GetBasicComment()
+	{
+
+		Faker<BasicCommentModel> commentsGenerator = new Faker<BasicCommentModel>()
+			.RuleFor(x => x.Id, Guid.NewGuid().ToString)
+			.RuleFor(c => c.Comment, f => f.Lorem.Sentence());
+
+		BasicCommentModel comment = commentsGenerator.Generate();
+
+		var basicComment = new BasicCommentModel(comment.Id, comment.Comment);
+
+		return basicComment;
+
+	}
+
+}
--- src/CoreBusiness/IssueTracker.CoreBusiness/GlobalUsings.cs
+++ src/CoreBusiness/IssueTracker.CoreBusiness/GlobalUsings.cs
@@ -1,2 +1,6 @@
-﻿global using MongoDB.Bson;
+﻿global using Bogus;
+
+global using IssueTracker.CoreBusiness.Models;
+
+global using MongoDB.Bson;
 global using MongoDB.Bson.Serialization.Attributes;
