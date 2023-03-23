e702ded9441cb4504daa74235ace614c53166693
mpaulosky <matthew.paulosky@gmail.com>
2023-03-23 13:34:28 -0700

    Add Solution Model to IssueTracker.CoreBusiness Project Closes #165

---
+++ src/CoreBusiness/IssueTracker.CoreBusiness/BogusFakes/FakeSolution.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//	File:		FakeSolution.cs
+//	Company:mpaulosky
+//	Author:	Matthew Paulosky
+//	Copyright (c) 2022. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace IssueTracker.CoreBusiness.BogusFakes;
+
+/// <summary>
+/// FakeSolution class
+/// </summary>
+public static class FakeSolution
+{
+
+	/// <summary>
+	/// Gets the new solution.
+	/// </summary>
+	/// <returns>SolutionModel</returns>
+	public static SolutionModel GetNewSolution()
+	{
+		Faker<SolutionModel> solutionGenerator = new Faker<SolutionModel>()
+		.RuleFor(x => x.Title, f => f.Lorem.Sentence())
+		.RuleFor(x => x.Description, f => f.Lorem.Sentence());
+		SolutionModel solution = solutionGenerator.Generate();
+		return solution;
+	}
+
+}
--- src/CoreBusiness/IssueTracker.CoreBusiness/Models/BasicSolutionModel.cs
+++ src/CoreBusiness/IssueTracker.CoreBusiness/Models/BasicSolutionModel.cs
@@ -1,39 +1,81 @@
 ﻿//-----------------------------------------------------------------------
-// <copyright File="BasicSolutionModel.cs"
-//	Company="mpaulosky">
-//	Author: Matthew Paulosky
+// <copyright>
+//	File:		BasicSolutionModel.cs
+//	Company:mpaulosky
+//	Author:	Matthew Paulosky
 //	Copyright (c) 2022. All rights reserved.
 // </copyright>
 //-----------------------------------------------------------------------
 
 namespace IssueTracker.CoreBusiness.Models;
 
+/// <summary>
+/// BasicSolutionModel class
+/// </summary>
+[Serializable]
 public class BasicSolutionModel
 {
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BasicSolutionModel"/> class.
+	/// </summary>
 	public BasicSolutionModel()
 	{
 	}
 
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BasicSolutionModel"/> class.
+	/// </summary>
+	/// <param name="solution">The solution.</param>
 	public BasicSolutionModel(SolutionModel solution)
 	{
 
 		Id = solution.Id;
-		SolutionTitle = solution.SolutionTitle;
-		Solution = solution.Solution;
+		Title = solution.Title;
+		Description = solution.Description;
 		Issue = solution.Issue;
 		Author = solution.Author;
 
 	}
 
+	/// <summary>
+	/// Gets or sets the identifier.
+	/// </summary>
+	/// <value>
+	/// The identifier.
+	/// </value>
 	public string Id { get; set; } = string.Empty;
 
-	public string SolutionTitle { get; set; } = string.Empty;
+	/// <summary>
+	/// Gets or sets the title.
+	/// </summary>
+	/// <value>
+	/// The title.
+	/// </value>
+	public string Title { get; set; } = string.Empty;
 
-	public string Solution { get; set; } = string.Empty;
+	/// <summary>
+	/// Gets or sets the description.
+	/// </summary>
+	/// <value>
+	/// The description.
+	/// </value>
+	public string Description { get; set; } = string.Empty;
 
+	/// <summary>
+	/// Gets or sets the issue.
+	/// </summary>
+	/// <value>
+	/// The issue.
+	/// </value>
 	public BasicIssueModel Issue { get; set; } = new BasicIssueModel();
 
+	/// <summary>
+	/// Gets or sets the author.
+	/// </summary>
+	/// <value>
+	/// The author.
+	/// </value>
 	public BasicUserModel Author { get; set; } = new();
 
 }
