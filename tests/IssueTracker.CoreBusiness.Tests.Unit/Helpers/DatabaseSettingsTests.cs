cc9ddb56b398987f8c6428541b512603d9c7c3fb
mpaulosky <matthew.paulosky@gmail.com>
2023-04-25 07:08:12 -0700

    Move Tests From IssueTracker.Library.Tests.Unit to IssueTracker.CoreBusiness.Tests.Unit

---
+++ tests/IssueTracker.CoreBusiness.Tests.Unit/Helpers/DatabaseSettingsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace IssueTracker.CoreBusiness.Helpers;
+
+[ExcludeFromCodeCoverage]
+public class DatabaseSettingsTests
+{
+
+	private DatabaseSettings CreateDatabaseSettings(string expectedCS, string expectedDbName)
+	{
+		return new DatabaseSettings(expectedCS, expectedDbName);
+	}
+
+	[Fact]
+	public void TestMethod1()
+	{
+
+		// Arrange
+		const string expectedCS = "ConnectionString";
+		const string expectedDbName = "DatabaseName";
+
+		// Act
+		var databaseSettings = CreateDatabaseSettings(expectedCS, expectedDbName);
+
+
+		// Assert
+		databaseSettings.ConnectionString.Should().Be(expectedCS);
+		databaseSettings.DatabaseName.Should().Be(expectedDbName);
+
+	}
+
+}
