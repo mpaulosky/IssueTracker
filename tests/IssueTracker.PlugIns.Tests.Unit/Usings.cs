c74a50e595acaad0ede81eddfb65d662e10fcd2e
mpaulosky <matthew.paulosky@gmail.com>
2023-05-11 13:16:55 -0700

    Cleanup Un-Necessary Tests and Remove Folders

--- tests/IssueTracker.PlugIns.Tests.Unit/Usings.cs
+++ tests/IssueTracker.PlugIns.Tests.Unit/Usings.cs
@@ -6,7 +6,6 @@ global using IssueTracker.CoreBusiness.BogusFakes;
 global using IssueTracker.CoreBusiness.Models;
 global using IssueTracker.PlugIns.Contracts;
 global using IssueTracker.PlugIns.DataAccess;
-global using IssueTracker.PlugIns.Helpers;
 global using IssueTracker.PlugIns.PlugInRepositoryInterfaces;
 
 global using Microsoft.Extensions.Caching.Memory;
@@ -23,4 +22,3 @@ global using Xunit;
 
 global using static IssueTracker.CoreBusiness.Helpers.CollectionNames;
 global using static IssueTracker.PlugIns.Tests.Unit.Fixtures.Fixtures;
-
