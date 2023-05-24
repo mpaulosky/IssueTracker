905b94242f88316107208c1fb183a9f3cefa37ea
mpaulosky <matthew.paulosky@gmail.com>
2023-05-24 11:13:57 -0700

    Cleanup RegisterConnections.cs

--- src/UI/IssueTracker.UI/Extensions/RegisterConnections.cs
+++ src/UI/IssueTracker.UI/Extensions/RegisterConnections.cs
@@ -5,8 +5,6 @@ public static partial class ServiceCollectionExtensions
 
 	public static IServiceCollection RegisterConnections(this IServiceCollection services, ConfigurationManager config)
 	{
-		string connectionString = config.GetSection("MongoDbSettings:ConnectionStrings").Value ?? string.Empty;
-		string databaseName = config.GetSection("MongoDbSettings:DatabaseName").Value ?? string.Empty;
 
 		services.AddSingleton<IDatabaseSettings, DatabaseSettings>();
 
